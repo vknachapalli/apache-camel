93d4db09121bf39cf30ac847650606958916d329
vknachapalli <vknachapalli>
2021-01-01 18:02:36 +0100

    add code to fetch customers

---
+++ rest-api-example/src/main/java/com/vknachapalli/rest/model/Customer.java
@@ -0,0 +1,38 @@
+package com.vknachapalli.rest.model;
+
+import java.time.Instant;
+
+public class Customer {
+    private int id;
+    private String name;
+    private Instant createdDateTime;
+
+    public Customer() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Instant getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(Instant createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+}
+
+
--- rest-api-example/src/main/java/com/vknachapalli/rest/router/RestRouter.java
+++ rest-api-example/src/main/java/com/vknachapalli/rest/router/RestRouter.java
@@ -1,10 +1,6 @@
 package com.vknachapalli.rest.router;
 
-import com.vknachapalli.rest.model.Message;
-import com.vknachapalli.rest.model.Request;
-import com.vknachapalli.rest.model.Response;
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
+import com.vknachapalli.rest.model.Customer;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.http.MediaType;
@@ -14,39 +10,17 @@ import org.springframework.stereotype.Component;
 public class RestRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        restConfiguration()
-                .component("servlet")
-                .bindingMode(RestBindingMode.auto);
+        restConfiguration().component("servlet").bindingMode(RestBindingMode.auto);
 
         rest()
                 .path("/api")
                 .consumes(MediaType.APPLICATION_JSON_VALUE)
                 .produces(MediaType.APPLICATION_JSON_VALUE)
-                .post()
-                .type(Request.class)
+                .get("/customers")
+                .type(Customer.class)
                 .route()
                 .log("Original Body: ${body}")
-                .split().simple("${body.getMessages}")
-                .log("Body parts: ${body}")
-                .process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        Message message = exchange.getMessage().getBody(Message.class);
-                        message.setMessage("Message is " + message.getMessage());
-                    }
-                })
-                .end()
-                .log("Aggregate ${body}")
-                .process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        Request request = exchange.getMessage().getBody(Request.class);
-                        Response response = new Response();
-                        response.setOutput("Thanks for submitting your request with input: '" + request.getInput() + "'");
-                        response.setMessages(request.getMessages());
-                        exchange.getMessage().setBody(response);
-                    }
-                })
+                .to("sql:SELECT * FROM customer")
                 .endRest();
     }
 }
